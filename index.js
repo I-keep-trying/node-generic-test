094c9d944dd6b4d9b4285aabe901dfca5e77a97e
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2022-08-17 15:00:54 -0400

    init

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ require('dotenv').config()
 const PORT = process.env.PORT || 8082
 
 const app = express()
+
+app.use((req, res, next) => {
+  res.setHeader('Permissions-Policy', 'geolocation=(), interest-cohort=()')
+  res.setHeader(
+    'Strict-Transport-Security',
+    'max-age=31536000;includeSubDomains'
+  )
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN')
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('Referrer-Policy', 'strict-origin')
+  res.setHeader('Cache-Control', 'max-age=31536000')
+  next()
+})
+
 app.use(express.static('build'))
 
 app.get('/ping', (req, res) => {
